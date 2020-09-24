e6be351042ebc659355fcfe856b0fd2f9fd9fde0
Jefry Romero <romerojefry@icloud.com>
2020-09-23 22:41:38 -0700

    add a question

---
+++ script.js
@@ -0,0 +1,12 @@
+var myQuestions = [
+    {
+        question: "What color is blueberry jam?",
+        answers: {
+            a: 'purple',
+            b: 'red',
+            c: 'blue'
+        },
+        correctAnswer: 'a'
+    },
+];
+//writing out my variable
