8c6cfbddf899f75394503925f23347e71e697a50
Jefry Romero <romerojefry@icloud.com>
2020-12-03 19:18:01 -0800

    adding a set timer

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var secondsLeft = 30
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft --;
+        timerInterval.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    },1000);
+}
+
 var myQuestions = [
     {
         question: "What color is blueberry jam?",
